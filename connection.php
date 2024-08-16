d30fde075156d956fdba7d0dac2fde38cd5868dc
Aroshana Dissanayake <aroshanaprabhathqualifications@gmail.com>
2024-08-16 16:58:27 +0530

    Connection Setup

--- connection.php
+++ connection.php
@@ -1,7 +1,47 @@
 <?php
 
+//Create Connection Class for Using project
+class connection{
+
+    public static $connection;
 
 
+    public static function setupConnection(){
+      
+        if (!isset(connection::$connection)){
+
+         connection:: $connection = new mysqli("localhost","root","DBMS@aroshana2024","e-commercewebapk","3306");
+
+        }
+    }
+
+
+    public static function iud($q){
+
+         connection::setupConnection();
+         connection::$connection->query($q);
+
+
+
+    }
+
+
+
+    public static function search($q){
+
+        connection::setupConnection();
+        $resultset =  connection::$connection->query($q);
+
+        return $resultset;
+
+
+
+    }
+
+
+
+
+}
 
 
 
